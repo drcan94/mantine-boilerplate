58bc00344638e018ab0b805fd89490cb5d86b6c4
Burak CAN <drcan94@gmail.com>
2023-03-08 06:14:03 +0300

    prettiering

--- src/components/Searching/SearchResult/styles/index.ts
+++ src/components/Searching/SearchResult/styles/index.ts
@@ -1,9 +1,14 @@
 import styled from "@emotion/styled";
-import { Text, TextProps, MantineTheme } from '@mantine/core';
+import { Text, TextProps, MantineTheme } from "@mantine/core";
 
-export const SearchResultContainer = styled(Text)<TextProps | {theme: MantineTheme}>`
+export const SearchResultContainer = styled(Text)<
+  TextProps | { theme: MantineTheme }
+>`
   padding: 10px 20px;
   &:hover {
-    background-color: ${({theme}) => theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.colors.gray[3]}; 
+    background-color: ${({ theme }) =>
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[7]
+        : theme.colors.gray[3]};
   }
 `;
